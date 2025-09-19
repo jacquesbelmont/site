81a66646847cf3aec74afb00f53da55efd2903d7
Jacques Belmont <jacquesbelmont@gmail.com>
2025-09-19 17:38:40 -0400

    server
    Altere a propriedade output de "hybrid" para "server".

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jacquesbelmont.com',
-  output: 'hybrid',
+  output: 'server', // [!code ++]
   adapter: node({
     mode: 'standalone'
   }),
